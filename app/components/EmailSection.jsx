737837ac67a4a2ea2cdb3e30154cf18855827011
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2024-10-21 12:59:51 +0200

    ✨feature: envoi de msg via le formulaire

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,43 +1,35 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import GithubIcon from "../../public/images/github-icon.svg";
 import LinkedinIcon from "../../public/images/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
 import WhatsAppButton from "./WhatsAppButton";
+import emailjs from "@emailjs/browser";
 
 const EmailSection = () => {
 	const [emailSubmitted, setEmailSubmitted] = useState(false);
+	const form = useRef();
 
 	const handleSubmit = async e => {
 		e.preventDefault();
-		const data = {
-			email: e.target.email.value,
-			subject: e.target.subject.value,
-			message: e.target.message.value,
-		};
-		const JSONdata = JSON.stringify(data);
-		const endpoint = "/api/send";
 
-		// Form the request for sending data to the server.
-		const options = {
-			// The method is POST because we are sending data.
-			method: "POST",
-			// Tell the server we're sending JSON.
-			headers: {
-				"Content-Type": "application/json",
-			},
-			// Body of the request is the JSON data we created above.
-			body: JSONdata,
-		};
-
-		const response = await fetch(endpoint, options);
-		const resData = await response.json();
-
-		if (response.status === 200) {
-			console.log("Message sent.");
-			setEmailSubmitted(true);
-		}
+		emailjs
+			.sendForm(
+				process.env.REACT_APP_EMAILJS_SERVICE_ID,
+				process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+				form.current,
+				process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+			)
+			.then(
+				result => {
+					console.log("Message sent.", result.text);
+					setEmailSubmitted(true);
+				},
+				error => {
+					console.log("FAILED...", error.text);
+				}
+			);
 	};
 
 	return (
@@ -46,7 +38,7 @@ const EmailSection = () => {
 			className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
 		>
 			<div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-			<div className="">
+			<div>
 				<h5 className="text-xl font-bold text-white my-2">
 					Faisons connaissance !
 				</h5>
@@ -77,7 +69,7 @@ const EmailSection = () => {
 						Email sent successfully!
 					</p>
 				) : (
-					<form className="flex flex-col" onSubmit={handleSubmit}>
+					<form ref={form} className="flex flex-col" onSubmit={handleSubmit}>
 						<div className="mb-6">
 							<label
 								htmlFor="email"
@@ -86,7 +78,7 @@ const EmailSection = () => {
 								Votre email
 							</label>
 							<input
-								name="email"
+								name="user_email"
 								type="email"
 								id="email"
 								required
